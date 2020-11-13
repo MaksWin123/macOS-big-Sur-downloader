25617573a022dc520cced11d915991d5b524f5f1
MaksWin123 <59917731+MaksWin123@users.noreply.github.com>
2020-11-13 21:08:57 +0100

    Add files via upload

---
+++ macOSBigSurInstaller.py
@@ -0,0 +1,18 @@
+import time
+import os
+
+
+print ("downloading macOS Big Sur 11.0 release...")
+time.sleep(15)
+print ("1GB/8GB")
+time.sleep(900)
+print ("5GB/8GB")
+time.sleep(900)
+print ("8GB/8GB")
+time.sleep(5)
+print ("changing boot priority")
+time.sleep(4) 
+
+print ("macOS has been successfully installed. Your mac will reboot automatically in 15 seconds.")
+time.sleep(15)
+os.system("sudo reboot")
